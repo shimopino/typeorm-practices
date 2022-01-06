004148962cbd7a8d3d898dc3342dde88d22e83ee
shimopino <shimoke4869@gmail.com>
2022-01-07 05:23:08 +0900

    feat: テーブルクエリの別名指定の方法を追加

--- src/queries/select-group-by-user.ts
+++ src/queries/select-group-by-user.ts
@@ -8,19 +8,22 @@ export const selectGroupByUser = async () => {
    * Implicit
    */
   const postRepository = connection.getRepository(GroupExplicit);
+  // ここで指定している名称は以下が該当する
+  //                                    ↓
+  // SELECT * FROM group_explicit group_explicit
   const queryBuilder = postRepository.createQueryBuilder("group_explicit");
 
   const posts = await queryBuilder
-    .innerJoin("group_explicit.userGroups", "userGroup")
+    .innerJoin("group_explicit.userGroups", "user_group")
     .select()
-    .where("userGroup.userId = :userId", { userId: 1 })
+    .where("user_group.userId = :userId", { userId: 1 })
     .getMany();
 
   console.log(`
-        SELECT * FROM group_explicit
-        INNER JOIN user_group_explicit userGroup
-        ON userGroup.groupId = group_explicit.id
-        WHERE userGroup.userId = 1;
+        SELECT * FROM group_explicit group_explicit
+        INNER JOIN user_group_explicit user_group
+        ON user_group.groupId = group_explicit.id
+        WHERE user_group.userId = 1;
     `);
   console.log(posts);
 };
